da7f0d2d71e61f6603130328d616dfc8856f898c
woojoung <ywj981217@naver.com>
2021-11-17 15:57:25 +0900

    scss

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-const emailInputEl =document.querySelectorAll('#exampleInputEmail1')
-const ModalEl =document.querySelector('#exampleModal')
-
-
-
-ModalEl.addEventListener('shown.bs.modal',function(){
-  emailInputEl.hide();
-})
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
